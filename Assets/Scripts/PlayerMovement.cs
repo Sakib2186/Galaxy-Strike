5d667b1c2dfa614cc58aa7ca909db9f56c19a42f
Sakib2186 <skmdsakib2186@gmail.com>
2026-01-25 21:13:17 +0600

    Learned about Mathf.Clamp

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float controlSpeed;
+    [SerializeField] float xClampPos;
+    [SerializeField] float yClampPos;
     Vector2 movement;
 
     // Update is called once per frame
@@ -19,8 +22,14 @@ public class PlayerMovement : MonoBehaviour
     private void ProcessTranlation()
     {
         float xOffset = movement.x * controlSpeed * Time.deltaTime;
+        float xRawPos = transform.localPosition.x + xOffset;
+        float xClamped = Mathf.Clamp(xRawPos, -xClampPos, xClampPos);
+
         float yOffset = movement.y * controlSpeed * Time.deltaTime;
-        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0f);
+        float yRawPos = transform.localPosition.y + yOffset;
+        float yClamped = Mathf.Clamp(yRawPos, -yClampPos, yClampPos);
+
+        transform.localPosition = new Vector3(xClamped,yClamped,0f);
     }
     // added comment
 }
